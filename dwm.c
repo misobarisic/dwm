9eedfb11e71c34f8ebf9c559ecf13907f445b389
Mišo Barišić <miki.barisic5@gmail.com>
2021-08-13 15:12:21 +0200

    typo

--- dwm.c
+++ dwm.c
@@ -1466,8 +1466,8 @@ void run(void) {
 }
 
 void runAutostart(void) {
-  system("cd ~/config/dwm; ./autostart_blocking.sh");
-  system("cd ~/config/dwm; ./autostart.sh &");
+  system("cd ~/.config/dwm; ./autostart_blocking.sh");
+  system("cd ~/.config/dwm; ./autostart.sh &");
 }
 
 void scan(void) {
