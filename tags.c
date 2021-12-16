9ca392b672de36513b787a1efb05ec21c84aade8
Mišo Barišić <miki.barisic5@gmail.com>
2021-12-16 23:07:02 +0100

    refactor

--- tags.c
+++ tags.c
@@ -1,104 +1,104 @@
 void relativemove(const Arg *arg) {
-  if (selmon->sel && arg->i) {
-    int endtag = selmon->selectedtag + arg->i;
-    if (endtag < 0 || endtag > LENGTH(tags) - 1)
-      return;
-    selmon->sel->tags = 1 << endtag;
-    focus(NULL);
-    arrange(selmon);
-  }
+    if (selmon->sel && arg->i) {
+        int endtag = selmon->selectedtag + arg->i;
+        if (endtag < 0 || endtag > LENGTH(tags) - 1)
+            return;
+        selmon->sel->tags = 1 << endtag;
+        focus(NULL);
+        arrange(selmon);
+    }
 }
 
 void relativeview(const Arg *arg) {
-  int i;
-  unsigned int tmptag;
+    int i;
+    unsigned int tmptag;
 
-  int newmon = selmon->selectedtag + arg->i;
-  if (newmon < 0 || newmon > LENGTH(tags) - 1)
-    return;
-  int endtag = 1 << newmon;
-  if ((endtag & TAGMASK) == selmon->tagset[selmon->seltags])
-    return;
-  selmon->seltags ^= 1;
-  if (endtag & TAGMASK) {
-    selmon->tagset[selmon->seltags] = endtag & TAGMASK;
-    selmon->tagdata->prevtag = selmon->tagdata->curtag;
+    int newmon = selmon->selectedtag + arg->i;
+    if (newmon < 0 || newmon > LENGTH(tags) - 1)
+        return;
+    int endtag = 1 << newmon;
+    if ((endtag & TAGMASK) == selmon->tagset[selmon->seltags])
+        return;
+    selmon->seltags ^= 1;
+    if (endtag & TAGMASK) {
+        selmon->tagset[selmon->seltags] = endtag & TAGMASK;
+        selmon->tagdata->prevtag = selmon->tagdata->curtag;
 
-		if (endtag == ~0)
-			selmon->tagdata->curtag = 0;
-		else {
-			for (i = 0; !(endtag& 1 << i); i++) ;
-			selmon->tagdata->curtag = i + 1;
-		}
-	} else {
-		tmptag = selmon->tagdata->prevtag;
-		selmon->tagdata->prevtag = selmon->tagdata->curtag;
-		selmon->tagdata->curtag = tmptag;
-	}
+        if (endtag == ~0)
+            selmon->tagdata->curtag = 0;
+        else {
+            for (i = 0; !(endtag & 1 << i); i++);
+            selmon->tagdata->curtag = i + 1;
+        }
+    } else {
+        tmptag = selmon->tagdata->prevtag;
+        selmon->tagdata->prevtag = selmon->tagdata->curtag;
+        selmon->tagdata->curtag = tmptag;
+    }
 
-	selmon->nmaster = selmon->tagdata->nmasters[selmon->tagdata->curtag];
-	selmon->mfact = selmon->tagdata->mfacts[selmon->tagdata->curtag];
-	selmon->sellt = selmon->tagdata->sellts[selmon->tagdata->curtag];
-	selmon->lt[selmon->sellt] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt];
-	selmon->lt[selmon->sellt^1] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt^1];
+    selmon->nmaster = selmon->tagdata->nmasters[selmon->tagdata->curtag];
+    selmon->mfact = selmon->tagdata->mfacts[selmon->tagdata->curtag];
+    selmon->sellt = selmon->tagdata->sellts[selmon->tagdata->curtag];
+    selmon->lt[selmon->sellt] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt];
+    selmon->lt[selmon->sellt ^ 1] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt ^ 1];
 
-	if (selmon->showbar != selmon->tagdata->showbars[selmon->tagdata->curtag])
-		togglebar(NULL);
+    if (selmon->showbar != selmon->tagdata->showbars[selmon->tagdata->curtag])
+        togglebar(NULL);
 
-  selmon->selectedtag = newmon;
-  focus(NULL);
-  arrange(selmon);
+    selmon->selectedtag = newmon;
+    focus(NULL);
+    arrange(selmon);
 }
 
 void relativemoveview(const Arg *arg) {
-  int i;
-  unsigned int tmptag;
+    int i;
+    unsigned int tmptag;
 
-  if (selmon->sel && arg->i) {
-    int endtag = selmon->selectedtag + arg->i;
-    if (endtag < 0 || endtag > LENGTH(tags) - 1)
-      return;
-    selmon->sel->tags = 1 << endtag;
-  }
-  int newmon = selmon->selectedtag + arg->i;
-  int endtag = 1 << newmon;
-  if (endtag & TAGMASK) {
-    selmon->tagset[selmon->seltags] = endtag & TAGMASK;
-    selmon->tagdata->prevtag = selmon->tagdata->curtag;
+    if (selmon->sel && arg->i) {
+        int endtag = selmon->selectedtag + arg->i;
+        if (endtag < 0 || endtag > LENGTH(tags) - 1)
+            return;
+        selmon->sel->tags = 1 << endtag;
+    }
+    int newmon = selmon->selectedtag + arg->i;
+    int endtag = 1 << newmon;
+    if (endtag & TAGMASK) {
+        selmon->tagset[selmon->seltags] = endtag & TAGMASK;
+        selmon->tagdata->prevtag = selmon->tagdata->curtag;
 
-		if (endtag == ~0)
-			selmon->tagdata->curtag = 0;
-		else {
-			for (i = 0; !(endtag& 1 << i); i++) ;
-			selmon->tagdata->curtag = i + 1;
-		}
-	} else {
-		tmptag = selmon->tagdata->prevtag;
-		selmon->tagdata->prevtag = selmon->tagdata->curtag;
-		selmon->tagdata->curtag = tmptag;
-	}
+        if (endtag == ~0)
+            selmon->tagdata->curtag = 0;
+        else {
+            for (i = 0; !(endtag & 1 << i); i++);
+            selmon->tagdata->curtag = i + 1;
+        }
+    } else {
+        tmptag = selmon->tagdata->prevtag;
+        selmon->tagdata->prevtag = selmon->tagdata->curtag;
+        selmon->tagdata->curtag = tmptag;
+    }
 
-	selmon->nmaster = selmon->tagdata->nmasters[selmon->tagdata->curtag];
-	selmon->mfact = selmon->tagdata->mfacts[selmon->tagdata->curtag];
-	selmon->sellt = selmon->tagdata->sellts[selmon->tagdata->curtag];
-	selmon->lt[selmon->sellt] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt];
-	selmon->lt[selmon->sellt^1] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt^1];
+    selmon->nmaster = selmon->tagdata->nmasters[selmon->tagdata->curtag];
+    selmon->mfact = selmon->tagdata->mfacts[selmon->tagdata->curtag];
+    selmon->sellt = selmon->tagdata->sellts[selmon->tagdata->curtag];
+    selmon->lt[selmon->sellt] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt];
+    selmon->lt[selmon->sellt ^ 1] = selmon->tagdata->ltidxs[selmon->tagdata->curtag][selmon->sellt ^ 1];
 
-	if (selmon->showbar != selmon->tagdata->showbars[selmon->tagdata->curtag])
-		togglebar(NULL);
-    
-  selmon->selectedtag = newmon;
-  focus(NULL);
-  arrange(selmon);
+    if (selmon->showbar != selmon->tagdata->showbars[selmon->tagdata->curtag])
+        togglebar(NULL);
+
+    selmon->selectedtag = newmon;
+    focus(NULL);
+    arrange(selmon);
 }
 
 void viewtag(const Arg *arg) {
-  int endtag = 1 << arg->ui;
-  if (selmon->sel && endtag) {
-    selmon->sel->tags = endtag & TAGMASK;
-    selmon->selectedtag = arg->ui;
-    focus(NULL);
-    view(arg);
-    arrange(selmon);
-  }
+    int endtag = 1 << arg->ui;
+    if (selmon->sel && endtag) {
+        selmon->sel->tags = endtag & TAGMASK;
+        selmon->selectedtag = arg->ui;
+        focus(NULL);
+        view(arg);
+        arrange(selmon);
+    }
 }
--- tags.def.h
+++ tags.def.h
@@ -1,4 +1,2 @@
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8" };
-/*static const char *tags[] = { "", "", "", "", "", "", "" };
-*/
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9","10" };
+//static const char *tags[] = { "", "", "", "", "", "", "", "" };
